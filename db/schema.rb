26be3eaa6f637d48e7ba33874da7e89eebcda0ab
Raphael Zimeray <raphaelzimeray@me.com>
2022-10-11 14:51:32 +0200

    Add address to Cars

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_11_124542) do
+ActiveRecord::Schema.define(version: 2022_10_11_125014) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 2022_10_11_124542) do
     t.string "model"
     t.float "latitude"
     t.float "longitude"
+    t.string "address"
   end
 
   create_table "users", force: :cascade do |t|
