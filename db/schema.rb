765ef40482806ad06d8ef77183b100a2ac0a6adf
Raphael Zimeray <raphaelzimeray@me.com>
2022-10-17 17:04:59 +0200

    style-in-index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_11_125014) do
+ActiveRecord::Schema.define(version: 2022_10_17_144521) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 2022_10_11_125014) do
     t.float "latitude"
     t.float "longitude"
     t.string "address"
+    t.integer "price"
   end
 
   create_table "users", force: :cascade do |t|
